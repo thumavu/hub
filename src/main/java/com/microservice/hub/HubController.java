87d0279420083e3e0ae8318aa0eced6736464754
Sizwe Mavuso <Sizwe.Mavuso@standardbank.co.za>
2023-06-05 02:07:24 +0200

    Docker is not dockerizing

--- src/main/java/com/microservice/hub/HubController.java
+++ src/main/java/com/microservice/hub/HubController.java
@@ -1,6 +1,5 @@
 package com.microservice.hub;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
